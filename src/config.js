c36be4c0528d3deb3c566bbe5aa3e9de4b2e430d
linkxiao <linkxiao@icloud.com>
2015-10-07 15:21:09 +0800

    1>update home

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@
  //path:页面路由，action:对应模块的主js文件(相当于main)，即：当前路由对应的js主文件
 define(function (require) {
     return [
-      {path: '/home/index', action: require('./home/index')},
+      {path: '/home/index', action: require('./home/index')},//首页
       {path: '/item/item', action: require('./item/index')},//二级页面
       {path: '/detail/detail', action: require('./detail/detail')},//三级详情页
       {path: '/other/other', action: require('./other/other')},//其他页面，such as:关于/问题/指南/反馈等
