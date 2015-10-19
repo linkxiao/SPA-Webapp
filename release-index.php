ace58c1740a103dc6fc619c26b7ed2c33a6caf64
linkxiao <linkxiao@icloud.com>
2015-10-19 22:32:00 +0800

    1>update bug

--- release-index.php
+++ release-index.php
@@ -14,9 +14,9 @@ $smarty->assign('tplData', array(
     "path"=>"./release/fe/heart/asset",//配置全局路径，方便部署上线
     'homeData'=>array(
         "bannerImg"=>array(//配置banner路径，方便以后对banner维护
-          0     => array('img' => '/src/common/img/banner1.jpg', 'link'=>'#/item/item~1'),
-          1     => array('img' => '/src/common/img/banner2.jpg', 'link'=>'#/item/item~2'),
-          2     => array('img' => '/src/common/img/banner3.jpg', 'link'=>'#/other/other~3'),
+          0     => array('img' => './release/fe/heart/asset/common/img/banner1.jpg', 'link'=>'#/item/item~1'),
+          1     => array('img' => './release/fe/heart/asset/common/img/banner2.jpg', 'link'=>'#/item/item~2'),
+          2     => array('img' => './release/fe/heart/asset/common/img/banner3.jpg', 'link'=>'#/other/other~3'),
         )
     ),
     'region'=> '新加坡'//动态配置region，方便以后扩展地域
